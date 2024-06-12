24abb51b0d2cabc929a382be269aa7e4a6149f87
azwefabless <andrew.wright@efabless.com>
2024-06-12 18:44:12 -0400

    drc fix
    Signed-off-by: azwefabless <andrew.wright@efabless.com>

--- drc/run_drc.sh
+++ drc/run_drc.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 magic -dnull -noconsole -rcfile /usr/share/pdk/sky130A/libs.tech/magic/sky130A.magicrc << EOF
-load ../mag/sky130_od_ip__tempsensor
+load ../mag/sky130_od_ip__tempsensor_ext_vp
 select top cell
 expand
 drc style drc(full)
@@ -11,7 +11,7 @@ drc count total
 set tk_version 8.5
 source \${CAD_ROOT}/magic/tcl/drcmgr.tcl
 #feedback save drc.log
-magic::drc_save_report sky130_od_ip__tempsensor drc.log
+magic::drc_save_report sky130_od_ip__tempsensor_ext_vp drc.log
 EOF
 exit 0
 
